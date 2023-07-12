fd8a9b4771ff8845282807416ade6be2b3c4fefe
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-07-12 19:44:18 +0200

    cleaned up the bootcamps controllers

--- backend/controllers/bootcampController.js
+++ backend/controllers/bootcampController.js
@@ -1,50 +1,41 @@
+//@desc     Get ALL bootcamps
+//@route    GET /api/v1/bootcamps
+//@access   Public
 const getAllBootcamps = (req, res, next) => {
-  try {
-    res.send({ status: 'OK', msg: 'Show all bootcamps. ppc' });
-  } catch (error) {
-    res
-      .status(error?.status || 500)
-      .send({ status: 'FAILED', data: { error: error?.message || error } });
-  }
+  res.status(200).json({ status: 'OK', msg: 'Display all bootcamps' });
 };
+
+//@desc     Get ONE bootcamp
+//@route    GET /api/v1/bootcamps/:id
+//@access   Public
 const getOneBootcamp = (req, res, next) => {
-  try {
-    res.send({ status: 'OK', msg: `Display bootcamp ${req.params.id}` });
-  } catch (error) {
-    res
-      .status(error?.status || 500)
-      .send({ status: 'FAILED', data: { error: error?.message || error } });
-  }
+  res
+    .status(200)
+    .json({ status: 'OK', msg: `Display bootcamp: ${req.params.id}` });
 };
 
+//@desc     Create a new bootcamp
+//@route    POST /api/v1/bootcamps
+//@access   Private
 const createOneBootcamp = (req, res, next) => {
-  try {
-    res.send({ status: 'OK', msg: 'Create a new bootcamp. ppc' });
-  } catch (error) {
-    res
-      .status(error?.status || 500)
-      .send({ status: 'FAILED', data: { error: error?.message || error } });
-  }
+  res.status(201).json({ status: 'OK', msg: 'Create a new bootcamp' });
 };
 
+//@desc     Update a bootcamp
+//@route    PUT /api/v1/bootcamps/:id
+//@access   Private
 const updateOneBootcamp = (req, res, next) => {
-  try {
-    res.send({ status: 'OK', msg: 'Updated a bootcamp.' });
-  } catch (error) {
-    res
-      .status(error?.status || 500)
-      .send({ status: 'FAILED', data: { error: error?.message || error } });
-  }
+  res
+    .status(200)
+    .json({ status: 'OK', msg: `Updated bootcamp: ${req.params.id}` });
 };
-
+//@desc     Delete a bootcamp
+//@route    DELETE /api/v1/bootcamps/:id
+//@access   Private
 const deleteABootcamp = (req, res, next) => {
-  try {
-    res.send({ status: 'OK', msg: `Delete a bootcamp: ${req.params.id}` });
-  } catch (error) {
-    res
-      .status(error?.status || 500)
-      .send({ status: 'FAILED', data: { error: error?.message || error } });
-  }
+  res
+    .status(200)
+    .json({ status: 'OK', msg: `Deleted bootcamp: ${req.params.id}` });
 };
 
 module.exports = {
