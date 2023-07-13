34d7c5aa7f11fabc50802af72a3881e4f0e41961
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-07-13 21:01:05 +0200

    Added err handling if mongoose drops out and connection to mongodb now works!

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const dotenv = require('dotenv');
 const v1BootcampRouter = require('./v1/routes/bootcampRoutes');
 const morgan = require('morgan');
-
+const connectDB = require('./config/db');
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
 
 const app = express();
 
@@ -17,7 +20,15 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/bootcamps', v1BootcampRouter);
 
 const PORT = process.env.PORT || 5000;
-app.listen(
+
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Unhandled Rejection (Error): ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
