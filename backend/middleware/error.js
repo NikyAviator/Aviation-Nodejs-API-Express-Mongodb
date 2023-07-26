69d697fc9b0da3058c55a887f0bd3548e5d27539
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-07-26 21:29:22 +0200

    added validation error in mongoose

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -5,12 +5,25 @@ const errorHandlers = (err, req, res, next) => {
   let error = { ...err };
   // sets the error message to the error object
   error.message = err.message;
-  console.log(err.stack.red);
+
+  // Log to console for dev
+  console.log(err);
 
   // Mongoose bad ObjectId
   if (err.name === 'CastError') {
     const message = `Resource not found with id of ${err.value}`;
     error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = `Duplicate field value entered`;
+    error = new ErrorResponse(message, 400);
+  }
+  // Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
   }
 
   res
