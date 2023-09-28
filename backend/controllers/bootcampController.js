72aa428fd072560ab8021310c85a210b8ea1bd81
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-09-28 18:36:21 +0200

    Populate, virtuals and cascade delete (doesn't work yet)

--- backend/controllers/bootcampController.js
+++ backend/controllers/bootcampController.js
@@ -31,7 +31,7 @@ const getAllBootcamps = asyncHandler(async (req, res, next) => {
     (match) => `$${match}`
   );
   // Finding resource
-  query = Bootcamp.find(JSON.parse(queryStr));
+  query = Bootcamp.find(JSON.parse(queryStr)).populate('courses');
 
   // Select Fields
   if (req.query.select) {
@@ -135,13 +135,15 @@ const updateOneBootcamp = asyncHandler(async (req, res, next) => {
 //@route    DELETE /api/v1/bootcamps/:id
 //@access   Private
 const deleteABootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
   if (!bootcamp) {
     return next(
       new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
     );
   }
+
+  bootcamp.remove(); // to trigger the remove middleware in the Bootcamp model
 
   res.status(200).json({ success: true, data: {} });
 });
