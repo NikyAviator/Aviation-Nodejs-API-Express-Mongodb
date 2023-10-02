e76ebfe92c7d8f195c2202f5207a0ae654a5f66c
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-10-02 15:09:04 +0200

    npm update & FIXED cascading delete!
    Things are properly deleted now when we delete a bootcamp, the appropriate courses are also deleted!

--- backend/models/Bootcamp.js
+++ backend/models/Bootcamp.js
@@ -136,11 +136,15 @@ BootcampSchema.pre('save', async function (next) {
 // Cascade delete courses when a bootcamp is deleted
 // this is a mongoose middleware
 // https://mongoosejs.com/docs/middleware.html
-BootcampSchema.pre('remove', async function (next) {
-  console.log(`Courses being removed from bootcamp ${this._id}`);
-  await this.model('Course').deleteMany({ bootcamp: this._id });
-  next();
-});
+BootcampSchema.pre(
+  'deleteOne',
+  { document: true, query: false },
+  async function (next) {
+    console.log(`Courses being removed from bootcamp ${this._id}`);
+    await this.model('Course').deleteMany({ bootcamp: this._id });
+    next();
+  }
+);
 
 // Reverse populate with virtuals
 // virtuals are properties that are not stored in MongoDB
