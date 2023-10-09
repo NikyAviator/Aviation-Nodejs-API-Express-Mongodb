7ce2ac978ae5767b55eece3011a31870ade1a81a
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-10-09 15:55:14 +0200

    User Login

--- backend/v1/routes/authRoutes.js
+++ backend/v1/routes/authRoutes.js
@@ -5,4 +5,6 @@ const router = express.Router();
 // Register a user
 router.post('/register', authController.registerUser);
 
+// Login a user
+router.post('/login', authController.login);
 module.exports = router;
