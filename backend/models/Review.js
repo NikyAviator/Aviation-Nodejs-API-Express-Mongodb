b56f43111fb70dd45c8039d25060af787b8713e8
Nikolai Kocev <nikolai_kocev@hotmail.com>
2023-10-26 16:53:04 +0200

    Added average score for the reviews

--- backend/models/Review.js
+++ backend/models/Review.js
@@ -37,4 +37,41 @@ const ReviewSchema = new mongoose.Schema({
 // Prevent user from submitting more than one review per bootcamp
 ReviewSchema.index({ bootcamp: 1, user: 1 }, { unique: true });
 
+// Static method to get avg rating and save
+ReviewSchema.statics.getAverageRating = async function (bootcampId) {
+  const obj = await this.aggregate([
+    // this is a mongoose method
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        // this is a mongodb method
+        _id: '$bootcamp',
+        averageRating: { $avg: '$rating' },
+      },
+    },
+  ]);
+  console.log(obj);
+
+  try {
+    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+      averageRating: obj[0].averageRating,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Call getAverageRating after save
+ReviewSchema.post('save', function () {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+
+// Call getAverageRating before remove
+// Could be a problem with remove, see Bootcamp.js middleware for fix
+ReviewSchema.pre('remove', function () {
+  this.constructor.getAverageRating(this.bootcamp);
+});
+
 module.exports = mongoose.model('Review', ReviewSchema);
